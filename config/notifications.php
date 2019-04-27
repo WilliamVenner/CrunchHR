93d088c625a69145db9c6afda1668cf9cea47d72
William Venner <william@venner.io>
2019-04-27 17:29:28 +0100

    Delete files

--- config/notifications.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-	'NOTIFICATION_PUBLIC_KEY'  => 'BC-DCTolZhJeQ1OJLEMvXVJ5ZEgcS3vCd9OOivEZ7fBQJaXy8s0nQDC6L53c-0MVCDzoIUOXOlN2eTHfEu--nCM',
-	'NOTIFICATION_PRIVATE_KEY' => '9rniMEDUnLF6ofpLfFGblvDEBEKrjrY5UOuK94pS-34',
-	'NOTIFICATION_ICON'        => 'https://crunchhr.venner.io/assets/img/notification.png',
-
-];
